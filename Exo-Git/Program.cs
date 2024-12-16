ac4155d223183c46d81514d6fa67be7845b1a9fa
TreDozier-hub <arnaud.dozier@gmail.com>
2024-12-16 15:14:25 +0100

    Update

---
+++ Exo-Git/Program.cs
@@ -0,0 +1,29 @@
+﻿///Exercice 8----------Quelle taille choisir-------------
+///
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int taille;
+            int poids;
+
+            Console.WriteLine("------------Quelle taille dois-je prendre--------------");
+
+            Console.Write("Entrez votre taille (en cm) : ");
+            double convertTaille = double.Parse(Console.ReadLine());
+
+            Console.Write("Entrez votre poids (en kg) : ");
+            double convertPoids = double.Parse(Console.ReadLine());
+
+            if
+        }
+    }
+}
+
+
